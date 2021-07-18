acb6682fbc566549b2c4d535ac8e6b0d33d8b513
arunprsh <arunprsh@amazon.com>
2021-07-18 21:40:28 +0000

    wip

--- src/deploy.py
+++ src/deploy.py
@@ -1,32 +1,37 @@
+import subprocess
+import sys
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'sagemaker'])
+
 from sagemaker.huggingface import HuggingFaceModel
 import sagemaker
 import argparse
 import boto3
-import time
 
 
-role = sagemaker.get_execution_role()
+if __name__ == '__main__':
+    # Parse argument variables passed via the DeployModel processing step
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_name', type=str)
+    parser.add_argument('--region', type=str)
+    parser.add_argument('--deployment_instance_type', type=str)
+    parser.add_argument('--deployment_instance_count', type=int)
+    parser.add_argument('--model_s3_path', type=str)
+    parser.add_argument('--endpoint_name', type=str)
+    args = parser.parse_args()
+    print(f'XXX: {args}')
+    
+    region = args.region
+    boto3.setup_default_session(region_name=region)
+    role = sagemaker.get_execution_role()
 
-# Parse argument variables passed via the DeployModel processing step
-parser = argparse.ArgumentParser()
-parser.add_argument('--model_name', type=str)
-parser.add_argument('--region', type=str)
-parser.add_argument('--deployment_instance_type', type=str)
-parser.add_argument('--deployment_instance_count', type=int)
-parser.add_argument('--model_s3_path', type=str)
-parser.add_argument('--endpoint_name', type=str)
-args = parser.parse_args()
 
-region = args.region
+    # Create HuggingFaceModel
+    huggingface_model = HuggingFaceModel(model_data=f'{args.model_s3_path}/model.tar.gz',  # path to your trained sagemaker model 
+                                         role=role, # iam role with permissions to create an Endpoint 
+                                         transformers_version='4.6', # transformers version used 
+                                         tensorflow_version='2.4', # pytorch version used 
+                                         py_version='py36', # python version of the DLC
+                                        )
 
-
-# Create HuggingFaceModel
-huggingface_model = HuggingFaceModel(model_data=f'{args.model_S3_path}/model.tar.gz',  # path to your trained sagemaker model 
-                                     role=role, # iam role with permissions to create an Endpoint 
-                                     transformers_version='4.6', # transformers version used 
-                                     pytorch_version='1.7', # pytorch version used 
-                                     py_version='py36', # python version of the DLC
-                                    )
-
-huggingface_model.deploy(initial_instance_count=args.deployment_instance_count, 
-                         instance_type=args.deployment_instance_type)
+    huggingface_model.deploy(initial_instance_count=args.deployment_instance_count, 
+                             instance_type=args.deployment_instance_type)
